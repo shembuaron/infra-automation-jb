eb24e5800a740d9fdf0ef000e6f7675775a3ef22
Shem <shembuaron@gmail.com>
2026-02-10 15:40:29 +0200

    First project files added. Logger created.

---
+++ src/log.py
@@ -0,0 +1,21 @@
+import logging
+import sys
+
+
+def setup_logging():
+    
+    logger = logging.getLogger("infra-auto")
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter("%(asctime)s %(levelname)s\t%(filename)s - %(funcName)s - %(message)s")
+
+
+    print_handler = logging.StreamHandler(sys.stdout)
+    print_handler.setLevel(logging.INFO)
+    file_handler = logging.FileHandler("../logs/provisioning.log")
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(formatter)
+
+
+    if not logger.handlers:
+        logger.addHandler(print_handler)
+        logger.addHandler(file_handler)
--- src/main.py
+++ src/main.py
@@ -1,6 +1,62 @@
-def main():
-    print("Hello from infra-automation-jb!")
+import logging
+from log import setup_logging
+import sys
+from pydantic import BaseModel, ValidationError
+import boto3
 
+
+#Setup logger
+setup_logging()
+logger = logging.getLogger("infra-auto")
+
+
+def get_user_input():
+    
+    # Get number of machines
+    while True:
+        machine_amount = input("Enter the amount of machines you want to privision:\n>>> ")
+        try:
+            machine_amount = int(machine_amount)
+            if machine_amount > 0:
+                break
+            else:
+                logger.info("Please enter a number greater than 0.")
+        except ValueError:
+            logger.info("Pleaes enter a valid number.")
+    
+    machines = []
+    
+    for i in range(machine_amount):
+        machine_name = input(f"Name for machine no. {i+1}:\n>>> ")
+        machine_os = input(f"OS for machine no. {i+1}:\n>>> ")
+        machine_cpu = input(f"CPU for machine no. {i+1}:\n>>> ")
+        machine_ram = input(f"RAM amount for machine no. {i+1}:\n>>> ")
+        machine_specs = {"name": machine_name, "os": machine_os, "cpu": machine_cpu, "ram": machine_ram}
+        
+        try:
+            machine = Machine.model_validate(machine_specs)
+            machines.append(machine)
+        except ValidationError as er:
+            print(er.errors()[0]["msg"])
+            i -= 1
+    
+    print(machines)
+
+
+class Machine(BaseModel):
+    name: str
+    os: str
+    cpu: str
+    ram: int
+
+
+def main():
+    
+    get_user_input()
+#     logger.info("info")
+#     logger.debug("debug")
+    
+    
 
 if __name__ == "__main__":
     main()
